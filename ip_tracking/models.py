5169b3d3eaadeb0b0d123d8474a0cccaf201bb02
Pritchad25 <pricts29@gmail.com>
2025-07-20 13:59:36 +0000

    Anomaly Detection.

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -1,11 +1,8 @@
 from django.db import models
 
-class RequestLog(models.Model):
-    ip_address = models.GenericIPAddressField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    path = models.CharField(max_length=255)
-    country = models.CharField(max_length=100, blank=True, null=True)
-    city = models.CharField(max_length=100, blank=True, null=True)
+class SuspiciousIP(models.Model):
+    ip_address = models.GenericIPAddressField(unique=True)
+    reason = models.TextField()
 
     def __str__(self):
-        return f"{self.ip_address} @ {self.timestamp} -> {self.path}"
+        return f"{self.ip_address} - {self.reason}"
